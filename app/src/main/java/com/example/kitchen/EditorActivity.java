df58c1e51e269477ce4bc9239c955c95e06d5ec7
shreyadenny <shreyadenny5@gmail.com>
2021-09-09 23:03:37 +0530

    editoractivity input

--- app/src/main/java/com/example/kitchen/EditorActivity.java
+++ app/src/main/java/com/example/kitchen/EditorActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,15 +20,41 @@ public class EditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
+        //name input
+        EditText editText_name = (EditText)findViewById(R.id.edit_ingredient_name);
+        String name = editText_name.getText().toString();
+
+        //quantity
+        TextView quantity = (TextView)findViewById(R.id.edit_quantity);
+        String q = "1";
+        quantity.setText(q);
+
+        //Spinner Values
         Spinner spinner = (Spinner) findViewById(R.id.spinner_gender);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.units_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        //spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) EditorActivity.this);
+
+        //Spinner input
+        String unit = spinner.getSelectedItem().toString();
+
+
+
+        if(unit=="gm") {
+            q = "100";
+            quantity.setText(q);
+        }
+        else if(unit=="ml")
+        {
+            q = "500";
+            quantity.setText(q);
+        }
 //        public void insertIngredient(){
 //            SQLiteDatabase db =
 //        }
@@ -40,5 +70,10 @@ public class EditorActivity extends AppCompatActivity {
 //                displayDatabaseInfo();
 //            }
 //        });
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
     }
 }
